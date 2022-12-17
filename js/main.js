0a230101af0b12a1a8fb1f4f29ad64e1b6c4c8c8
unknown <pavithrasivanoli@gmail.com>
2022-12-17 09:15:51 +0530

    updated the files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var age=parseInt(prompt("enter the age"));
+var test=prompt("enter the age");
+if((age>18)&& (test=="pass"))
+{
+	alert("person is eligible")
+	}
+	else if ((age>18)&& (test=="fail"))
+	{
+		alert("person is not eligible go again to the 1st round")
+	}		
+}
+else
+{
+	alert("age is not eligible for lisence")
+}
+
+		
+		
